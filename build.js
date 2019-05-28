21ae30c18d73648588c95afdccaa17bf290dd9d4
Eric Murphy <hi@ericnmurphy.com>
2019-05-28 20:27:53 +0700

    Added processing blog posts

--- build.js
+++ build.js
@@ -1,1 +1,22 @@
-console.log('Hello World');
+var fs = require('fs');
+
+// get all blog posts inside 'blog' directory
+fs.readdir('blog', function(err, files) {
+  if (err) throw err;
+
+  fs.mkdirSync('build/blog', {recursive: true});
+
+  // get template html
+  var template = fs.readFileSync('template.html', {encoding: 'utf8'});
+
+  // create blog posts
+  for (i = 0; i < files.length; i++) {
+    // get html from files
+    var path = 'blog/' + files[i];
+    var post = fs.readFileSync(path, {encoding: 'utf8'});
+    var html = template.replace('<!-- CONTENT -->', post);
+
+    // write finished file to build folder
+    fs.writeFileSync('build/' + path, html, {encoding: 'utf8'});
+  }
+});
