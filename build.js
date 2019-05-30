8f18aaf1a2afda42dd576a2d5b27d0d5297c6f28
Eric Murphy <hi@ericnmurphy.com>
2019-05-30 17:23:43 +0700

    Refactored code

--- build.js
+++ build.js
@@ -1,21 +1,12 @@
 var fs = require('fs');
 
-// get all blog posts inside 'blog' directory
-fs.readdir('blog', function(err, files) {
-  if (err) throw err;
-
-  // create directory
-  fs.mkdirSync('build/blog', {recursive: true});
-
-  // get template html
-  var template = fs.readFileSync('template.html', {encoding: 'utf8'});
-
+function createBlogRoll(posts) {
   //create list of posts for home page
   var blogroll = [];
 
   // get metadata for each blog post
-  for (i = 0; i < files.length; i++) {
-    var postPath = 'blog/' + files[i];
+  for (i = 0; i < posts.length; i++) {
+    var postPath = 'blog/' + posts[i];
     var postBody = fs.readFileSync(postPath, {encoding: 'utf8'});
     var postTitle = postBody.match(/<h1>(.*?)<\/h1>/)[1];
     var postDate = postBody.match(/<span class="date">(.*?)<\/span>/)[1];
@@ -43,25 +34,44 @@ fs.readdir('blog', function(err, files) {
         post.date +
         '</date></article>';
     });
+  return indexBlog;
+}
 
-  // create home page
+function createIndexPage(posts, template) {
+  // create index page
   var indexPath = 'index.html';
   var indexBody = fs
     .readFileSync(indexPath, {encoding: 'utf8'})
-    .replace('<!-- BLOG -->', indexBlog);
+    .replace('<!-- BLOG -->', createBlogRoll(posts));
   var indexHtml = template.replace('<!-- CONTENT -->', indexBody);
 
   // write finished home page to build folder
   fs.writeFileSync('build/' + indexPath, indexHtml, {encoding: 'utf8'});
+}
 
+function createBlogPages(posts, template) {
   // create blog posts
-  for (i = 0; i < files.length; i++) {
-    // get html from files
-    var path = 'blog/' + files[i];
+  for (i = 0; i < posts.length; i++) {
+    // get html from posts
+    var path = 'blog/' + posts[i];
     var post = fs.readFileSync(path, {encoding: 'utf8'});
     var html = template.replace('<!-- CONTENT -->', post);
 
     // write finished file to build folder
     fs.writeFileSync('build/' + path, html, {encoding: 'utf8'});
   }
+}
+
+// get all blog posts inside 'blog' directory
+fs.readdir('blog', function(err, files) {
+  if (err) throw err;
+
+  // create directory
+  fs.mkdirSync('build/blog', {recursive: true});
+
+  // get template html
+  var template = fs.readFileSync('template.html', {encoding: 'utf8'});
+
+  createIndexPage(files, template);
+  createBlogPages(files, template);
 });
