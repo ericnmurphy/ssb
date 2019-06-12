41be4d48e3234f2189c72559adf22a3d33cd2448
Eric Murphy <eric@ericmurphy.xyz>
2019-06-12 21:30:23 +0700

    Now grabbing blog posts for blogroll

--- build.js
+++ build.js
@@ -1,5 +1,8 @@
 const fs = require('fs');
 
+const srcDirectory = './src';
+const buildDirectory = './build';
+
 // if build directory already exists, delete it
 const deleteFolder = folder => {
   // get every file in folder
@@ -21,7 +24,7 @@ const deleteFolder = folder => {
 };
 
 // copy src to build before we build out the pages/posts
-const copyFolder = (source, destination) => {
+const buildFolder = (source, destination) => {
   // check if current file is a folder
   if (fs.statSync(source).isDirectory()) {
     // create folder in build directory
@@ -29,51 +32,57 @@ const copyFolder = (source, destination) => {
 
     // copy every file in directory
     fs.readdirSync(source).forEach(file => {
-      copyFolder(`${source}/${file}`, `${destination}/${file}`);
+      buildFolder(`${source}/${file}`, `${destination}/${file}`);
     });
+    // if it's a page . . .
+  } else if (source.indexOf('.html') > -1) {
+    buildPage(source, destination);
+    // if other file, jusy copy it
   } else {
     fs.copyFileSync(source, destination);
   }
 };
 
-// get template html
-const template = fs.readFileSync('template.html', {encoding: 'utf8'});
-const pages = [];
+const createBlogRoll = () => {
+  //create list of posts for home page
+  const blogroll = [];
 
-const getPages = folder => {
-  // get all files in directory
-  fs.readdirSync(folder).forEach(file => {
-    const filename = `${folder}/${file}`;
-
-    // if directory, go deeper another level
-    if (fs.statSync(filename).isDirectory()) {
-      getPages(filename);
-      // only grab html files
-    } else if (filename.indexOf('.html') > -1) {
-      pages.push(filename);
-    }
+  // get metadata for each blog post
+  fs.readdirSync(`${srcDirectory}/blog/`).forEach(post => {
+    const filename = `${srcDirectory}/blog/${post}`;
+    const relativeFilename = `blog/${post}`;
+    const postHtml = fs.readFileSync(filename, {encoding: 'utf8'});
+    const postTitle = postHtml.match(/<h1>(.*?)<\/h1>/)[1];
+    const postDate = postHtml.match(/<span class="date">(.*?)<\/span>/)[1];
+    const postMeta = {
+      title: postTitle,
+      path: relativeFilename,
+      date: postDate,
+    };
+    blogroll.push(postMeta);
   });
+
+  return blogroll;
+  console.log(blogroll);
 };
 
-const buildPages = pages => {
-  pages.forEach(page => {
-    const pageHtml = fs.readFileSync(page, {encoding: 'utf8'});
-    const newPageHtml = template.replace('<!-- CONTENT -->', pageHtml);
-    fs.writeFileSync(page, newPageHtml, {encoding: 'utf8'});
-  });
+// get template html
+const template = fs.readFileSync('template.html', {encoding: 'utf8'});
+
+const buildPage = (source, destination) => {
+  const pageHtml = fs.readFileSync(source, {encoding: 'utf8'});
+  const newPageHtml = template.replace('<!-- CONTENT -->', pageHtml);
+  fs.writeFileSync(destination, newPageHtml, {encoding: 'utf8'});
 };
 
 const build = () => {
-  const srcDirectory = './src';
-  const buildDirectory = './build';
-
   // delete build folder if exists already
   if (fs.existsSync(buildDirectory)) {
     deleteFolder(buildDirectory);
   }
 
-  copyFolder(srcDirectory, buildDirectory);
-  buildPages(['./build/blog/first-post.html', './build/index.html']);
+  buildFolder(srcDirectory, buildDirectory);
 };
 
-build();
+// build();
+createBlogRoll();
