373cd4a8f3c39cc56a34575391c6a07058e24467
Eric Murphy <eric@ericmurphy.xyz>
2019-06-12 21:02:51 +0700

    Fixed file copy

--- build.js
+++ build.js
@@ -1,19 +1,18 @@
-const path = require('path');
 const fs = require('fs');
 
 // if build directory already exists, delete it
 const deleteFolder = folder => {
   // get every file in folder
   fs.readdirSync(folder).forEach(file => {
-    // get current folder
-    const currentFile = `${folder}/${file}`;
+    // get current folder/file
+    const filename = `${folder}/${file}`;
 
     // check if we need to go deeper
-    if (fs.statSync(currentFile).isDirectory()) {
-      deleteFolder(currentFile);
+    if (fs.statSync(filename).isDirectory()) {
+      deleteFolder(filename);
     } else {
       // else, delete file
-      fs.unlinkSync(currentFile);
+      fs.unlinkSync(filename);
     }
   });
 
@@ -21,6 +20,7 @@ const deleteFolder = folder => {
   fs.rmdirSync(folder);
 };
 
+// copy src to build before we build out the pages/posts
 const copyFolder = (source, destination) => {
   // check if current file is a folder
   if (fs.statSync(source).isDirectory()) {
@@ -29,11 +29,38 @@ const copyFolder = (source, destination) => {
 
     // copy every file in directory
     fs.readdirSync(source).forEach(file => {
-      copyFolder(path.join(source, file), path.join(destination, file));
+      copyFolder(`${source}/${file}`, `${destination}/${file}`);
     });
   } else {
-    fs.linkSync(source, destination);
+    fs.copyFileSync(source, destination);
   }
+};
+
+// get template html
+const template = fs.readFileSync('template.html', {encoding: 'utf8'});
+const pages = [];
+
+const getPages = folder => {
+  // get all files in directory
+  fs.readdirSync(folder).forEach(file => {
+    const filename = `${folder}/${file}`;
+
+    // if directory, go deeper another level
+    if (fs.statSync(filename).isDirectory()) {
+      getPages(filename);
+      // only grab html files
+    } else if (filename.indexOf('.html') > -1) {
+      pages.push(filename);
+    }
+  });
+};
+
+const buildPages = pages => {
+  pages.forEach(page => {
+    const pageHtml = fs.readFileSync(page, {encoding: 'utf8'});
+    const newPageHtml = template.replace('<!-- CONTENT -->', pageHtml);
+    fs.writeFileSync(page, newPageHtml, {encoding: 'utf8'});
+  });
 };
 
 const build = () => {
@@ -46,21 +73,7 @@ const build = () => {
   }
 
   copyFolder(srcDirectory, buildDirectory);
+  buildPages(['./build/blog/first-post.html', './build/index.html']);
 };
-
-function getPages(directory) {
-  // get all files in directory
-  var files = fs.readdirSync(directory);
-
-  for (var i = 1; i < files.length; i++) {
-    var filename = path.join(directory, files[i]);
-    var stat = fs.lstatSync(filename);
-    if (stat.isDirectory()) {
-      getPages(filename); //recurse
-    } else if (filename.indexOf('.html')) {
-      //      pages.push(filename);
-    }
-  }
-}
 
 build();
